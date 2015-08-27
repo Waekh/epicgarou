ccc71879741986a89ccd65c1f68515afd691e63c
Tino Nguyen <qttino@gmail.com>
2015-08-27 15:45:33 +0200

    Ajout fonctionnalité test (bouton commencer partie)

--- MainActivityFragment.java
+++ MainActivityFragment.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    private Button btnStart;
 
     public MainActivityFragment() {
     }
@@ -17,5 +21,16 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        btnStart = (Button) view.findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this.getActivity(),
+                "Partie commencée", Toast.LENGTH_LONG).show();
+    }
+}
